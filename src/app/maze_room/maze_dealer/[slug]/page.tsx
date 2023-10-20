140aefb75791fc58c1b0d69510e75b5dd888c95b
neko967 <113199838+neko967@users.noreply.github.com>
2023-10-21 02:58:44 +0900

    迷路の土台を整えた

--- src/app/maze_room/maze_dealer/[slug]/page.tsx
+++ src/app/maze_room/maze_dealer/[slug]/page.tsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import Image from "next/image";
-import localImage from "../../../../../public/3.png";
+import localImage from "../../../../../public/horror_image.png";
 
 type Position = {
   x: number;
@@ -10,21 +10,33 @@ type Position = {
 };
 
 export default function Dealer({ params }: { params: { slug: string } }) {
+  const elements = 20;
+  const keyImage = "/keyImage.png";
+  const [windowSize, setWindowSize] = useState({ width: window.innerWidth, height: window.innerHeight });
+  const cellSize = Math.min(windowSize.width, windowSize.height) / elements;
   const [ws, setWs] = useState<WebSocket | null>(null);
-  const [message, setMessage] = useState('');
   const router = useRouter();
   const [playerPosition, setPlayerPosition] = useState<Position>({ x: 0, y: 0 });
   const [isGameOver, setIsGameOver] = useState(false);
+  const [resetButton, setResetButton] = useState(false);
   const [isGameClear, setIsGameClear] = useState(false);
   const [isGameStarted, setIsGameStarted] = useState(false);
-  const [hasKey, setHasKey] = useState(false);
+  const [keys, setKeys] = useState({
+    key1: false,
+    key2: false,
+    key3: false
+  });
 
-  // 音声を再生する関数
-  const playGameOverSound = () => {
-    const sound = new Audio("/aaaa.wav");
-    sound.play();
-  };
-
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  
   useEffect(() => {
     // Move cursor to the starting position
     const startPosition = document.querySelector('[data-start]');
@@ -54,9 +66,20 @@ export default function Dealer({ params }: { params: { slug: string } }) {
         websocket.onmessage = (event) => {
           if (event.data == "gameover") {
             setIsGameOver(true);
-          }
-          if (event.data == "haskey") {
-            setHasKey(true);
+            playGameOverSound();
+            const timer = setTimeout(() => {
+              setResetButton(true);
+            }, 3000);
+            return () => clearTimeout(timer);
+          } else if (event.data == "getkey1") {
+            setKeys(prev => ({ ...prev, key1: true }));
+            maze[12][7] = ' ';
+          } else if (event.data == "getkey2") {
+            setKeys(prev => ({ ...prev, key2: true }));
+            maze[5][17] = ' ';
+          } else if (event.data == "getkey3") {
+            setKeys(prev => ({ ...prev, key3: true }));
+            maze[14][23] = ' ';
           }
         };
 
@@ -69,50 +92,66 @@ export default function Dealer({ params }: { params: { slug: string } }) {
     checkRoomExists();
   }, [params.slug]);
 
-  useEffect(() => {
-    if (isGameOver && ws && ws.readyState === WebSocket.OPEN) {
-      playGameOverSound();
-    }
-  }, [isGameOver]);
+  const playGameOverSound = () => {
+    const sound = new Audio("/horror_sound.wav");
+    sound.play();
+  };
 
   const restartGame = () => {
     setIsGameOver(false);
+    setResetButton(false);
     setIsGameStarted(false);
-    setHasKey(false);
+    setKeys({
+      key1: false,
+      key2: false,
+      key3: false
+    });
     // 他に初期化するべきステートや変数があればこちらに追加
   };
 
   const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
     const rect = e.currentTarget.getBoundingClientRect();
-    const x = Math.floor((e.clientX - rect.left) / 40);
-    const y = Math.floor((e.clientY - rect.top) / 40);
+    const x = Math.floor((e.clientX - rect.left) / cellSize);
+    const y = Math.floor((e.clientY - rect.top) / cellSize);
 
-    if (maze[y][x] === '#' || (maze[y][x] === '*' && !hasKey) ) {
-      if (ws && ws.readyState === WebSocket.OPEN) {
+    if (ws && ws.readyState === WebSocket.OPEN) {
+      if (maze[y][x] === '#' || (maze[y][x] === '*' && !keys) ) {
         ws.send('gameover');
+      } else if (maze[y][x] === 'G') {
+        setIsGameClear(true);
+      } else if (y === 15 && x === 5 && maze[y][x] === 'K') {
+        ws.send('getkey1');
+      } else if (y === 6 && x === 5 && maze[y][x] === 'K') {
+        ws.send('getkey2');
+      } else if (y === 13 && x === 24 && maze[y][x] === 'K') {
+        ws.send('getkey3');
+      } else {
+        setPlayerPosition({ x, y });
       }
-    } else if (maze[y][x] === 'G') {
-      setIsGameClear(true);
-    } else if (maze[y][x] === 'K') {  // 2. プレイヤーがキーに当たった場合、hasKey を true に設定
-      if (ws && ws.readyState === WebSocket.OPEN) {
-        ws.send('haskey');
-      }
-    } else {
-      setPlayerPosition({ x, y });
     }
   };
 
   const maze = [
-    ['S', ' ', ' ', ' ', '#', '#', '#', '#', '#', '#'],
-    ['#', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', '#'],
-    ['#', ' ', '#', '#', '#', '#', '#', ' ', '#', '#'],
-    ['#', ' ', '#', ' ', ' ', ' ', '#', ' ', '#', '#'],
-    ['#', ' ', '#', ' ', '#', ' ', '#', ' ', '#', '#'],
-    ['#', ' ', '#', ' ', '#', ' ', '#', ' ', '#', '#'],
-    ['#', ' ', '#', ' ', '#', ' ', ' ', ' ', '#', '#'],
-    ['#', ' ', ' ', ' ', '#', '#', '#', ' ', ' ', 'K'],
-    ['#', '#', '#', '#', '#', '#', '#', '#', '*', '#'],
-    ['#', '#', '#', '#', '#', '#', '#', '#', 'G', '#'],
+    ['S', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#', '#', '#'],
+    ['#', ' ', '#', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', '#', '#', '#', '#', ' ', ' ', ' ', '#', '#'],
+    ['#', ' ', '#', ' ', '#', ' ', '#', ' ', '#', '#', '#', ' ', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#'],
+    ['#', ' ', ' ', ' ', '#', ' ', '#', ' ', '#', '#', '#', ' ', ' ', ' ', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#'],
+    ['#', ' ', '#', ' ', '#', ' ', ' ', ' ', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#'],
+    ['#', ' ', '#', ' ', '#', '#', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '*', ' ', ' ', ' ', ' ', ' ', ' ', '#'],
+    ['#', ' ', '#', ' ', '#', 'K', ' ', ' ', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#'],
+    ['#', ' ', '#', ' ', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', ' ', ' ', ' ', ' ', ' ', '#', '#', '#'],
+    ['#', ' ', '#', ' ', '#', ' ', ' ', ' ', '#', '#', ' ', ' ', ' ', ' ', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#'],
+    ['#', ' ', '#', ' ', ' ', ' ', '#', ' ', '#', '#', '#', '#', '#', ' ', '#', '#', '#', ' ', ' ', ' ', ' ', ' ', '#', '#', '#'],
+    ['#', ' ', '#', ' ', '#', ' ', '#', ' ', '#', '#', '#', ' ', ' ', ' ', '#', '#', '#', '#', '#', '#', '#', ' ', '#', '#', '#'],
+    ['#', ' ', '#', ' ', '#', ' ', '#', ' ', '#', '#', '#', ' ', '#', '#', '#', '#', '#', ' ', ' ', ' ', ' ', ' ', '#', '#', '#'],
+    ['#', ' ', '#', ' ', '#', ' ', ' ', '*', '#', '#', '#', ' ', '#', '#', '#', '#', '#', ' ', '#', '#', '#', '#', '#', '#', '#'],
+    ['#', ' ', ' ', ' ', '#', '#', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'K'],
+    ['#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '*', '#'],
+    ['#', ' ', ' ', ' ', ' ', 'K', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', 'G', '#'],
+    ['#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
+    ['#', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '*', '#'],
+    ['#', ' ', ' ', ' ', ' ', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', 'G', '#'],
+    ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
   ];
 
   return (
@@ -126,9 +165,11 @@ export default function Dealer({ params }: { params: { slug: string } }) {
       ) :isGameOver ? (
         <div>
           <Image src={localImage} alt="ホラー" />
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-5 rounded-lg shadow-lg">
-            <button onClick={restartGame} className="bg-blue-500 text-white px-4 py-2 rounded">やり直し</button>
-          </div>
+          {resetButton && 
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-5 rounded-lg shadow-lg">
+              <button onClick={restartGame} className="bg-blue-500 text-white px-4 py-2 rounded">やり直し</button>
+            </div>
+          }
         </div>
       ) : isGameClear ? (
         <div style={{ fontSize: '24px', color: 'green' }}>ゲームクリア</div>
@@ -136,9 +177,14 @@ export default function Dealer({ params }: { params: { slug: string } }) {
         <div
           style={{
             display: 'grid',
-            gridTemplateColumns: 'repeat(10, 40px)',
+            gridTemplateColumns: `repeat(25, ${cellSize}px)`,
             cursor: 'none',
-            gridGap: '0px'
+            gridGap: '0px',
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+            top: 0,
+            left: 0
           }}
           onMouseMove={handleMouseMove}
         >
@@ -148,17 +194,30 @@ export default function Dealer({ params }: { params: { slug: string } }) {
                 key={`${rowIndex}-${cellIndex}`}
                 data-start={cell === 'S' ? 'true' : undefined}
                 style={{
-                  boxSizing: 'border-box',
-                  width: '40px',
-                  height: '40px',
-                  backgroundColor:
-                    cell === '#' ? 'black' :
-                    cell === 'S' ? 'green' :
-                    cell === 'G' ? 'red' :
-                    cell === 'K' ? (hasKey ? 'white' : 'gold') :  // キーを取得した後は、キーの位置を白に
-                    cell === '*' ? (hasKey ? 'white' : 'silver') :  // 3. キーを取得した場合、障害物 * が消えるように
-                    'white',
-                  cursor: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="5" height="5" viewBox="0 0 2 2"><circle cx="1" cy="1" r="1" fill="black" /></svg>') 1 1, auto`
+                    boxSizing: 'border-box',
+                    width: `${cellSize}px`,
+                    height: `${cellSize}px`,
+                    backgroundColor:
+                        cell === '#' ? 'black' :
+                        cell === 'S' ? 'green' :
+                        cell === 'G' ? 'red' :
+                        cell === 'K' ? (
+                          rowIndex === 15 && cellIndex === 5 && keys.key1 ? 'white' :
+                          rowIndex === 6 && cellIndex === 5 && keys.key2 ? 'white' :
+                          rowIndex === 13 && cellIndex === 24 && keys.key3 ? 'white' : 'gold'
+                        ) :
+                        cell === '*' ? (
+                          rowIndex === 12 && cellIndex === 7 && keys.key1 ? 'white' :
+                          rowIndex === 5 && cellIndex === 17 && keys.key2 ? 'white' :
+                          rowIndex === 14 && cellIndex === 23 && keys.key3 ? 'white' : 'silver'
+                        ) :
+                        'white',
+                      cursor: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="5" height="5" viewBox="0 0 2 2"><circle cx="1" cy="1" r="1" fill="black" /></svg>') 1 1, auto`,
+                      backgroundSize: 'cover',
+                      backgroundImage: cell === 'K' ? rowIndex === 15 && cellIndex === 5 && !keys.key1 ? `url(${keyImage})` :
+                                                      rowIndex === 6 && cellIndex === 5 && !keys.key2 ? `url(${keyImage})` :
+                                                      rowIndex === 13 && cellIndex === 24 && !keys.key3 ? `url(${keyImage})` : undefined
+                                                    : undefined
                 }}
               ></div>
             ))
