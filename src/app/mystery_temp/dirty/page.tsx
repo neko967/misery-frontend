fbf0a3c4ee5a9ca62e38aea7ea9bf736a371a12c
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2023-10-25 22:43:38 +0900

    mystery_tempという一時作業ディレクトリを作成

---
+++ src/app/mystery_temp/dirty/page.tsx
@@ -0,0 +1,20 @@
+import { useRouter } from 'next/navigation';
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div
+      className="h-screen w-full bg-cover"
+      style={{ backgroundImage: "url('/background.png')" }}
+    >
+      <h1 className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-orange-600 p-5 rounded-lg shadow-lg">
+        謎解きの作業場所 app/mystery_temp/dirty/page.tsx
+      </h1>
+      <Link href="/maze/dirty" className="absolute top-2/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-red-600 p-5 rounded-lg shadow-lg">
+        <button>部屋から出る</button>
+      </Link>
+    </div>
+  );
+}
+
+
