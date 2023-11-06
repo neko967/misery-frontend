814605e6a2b4f731420fd4a1f8f98df24fdba776
rika <maxdeul@gmail.com>
2023-11-06 23:49:38 +0900

    prologueのguest/hostno文章作成

--- src/app/prologue/host/[slug]/page.tsx
+++ src/app/prologue/host/[slug]/page.tsx
@@ -6,8 +6,6 @@ import TextEffect from '../../components/TextEffect';
 import styles from '../../components/WaitingRoomButton.module.css';
 import DirtyButtonStyles from '../../components/WaitingRoomDirtyButton.module.css';
 
-
-
 function WaitingRoomButton() {
   return (
       <main className={styles.container}>
@@ -20,6 +18,7 @@ function WaitingRoomButton() {
       </main>
   );
 }
+
 function WaitingRoomDirtyButton() {
   return (
       <main className={DirtyButtonStyles.container}>
@@ -32,6 +31,7 @@ function WaitingRoomDirtyButton() {
       </main>
   );
 }
+
 type RoomState = 'clean' | 'dirty';
 
 export default function Home({ params }: { params: { slug: string } }) {
@@ -39,6 +39,19 @@ export default function Home({ params }: { params: { slug: string } }) {
   const router = useRouter();
   const [hostWhichRoom, setHostWhichRoom] = useState<RoomState>();
   const [guestWhichRoom, setGuestWhichRoom] = useState<RoomState>();
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+
+  const storyTexts = [
+    '二人の探検者は手分けして探索することを決めた。一方が綺麗な部屋へ、もう一方は寂れた部屋へと足を進める。',
+    '綺麗な部屋には時間が止まったかのような静寂が漂い、寂れた部屋は過去の栄華を色濃く残す荒廃が存在した。',
+    '二つの部屋は異なる物語と未解の謎を秘め、探検者たちはそれぞれの扉を静かに開ける。',
+    '綺麗な部屋へ進んだ探検者は整然と保たれた空間で手がかりを求め、寂れた部屋に入った者は壁に投げかけられた自分の影と風の唸りに耳を傾けた。',
+    'やがて、彼らが再会する時、それぞれの経験は一つの大きな物語を紡ぎ、洋館の謎を解き明かす鍵となるだろう。二人の旅は、このとき始まりを告げる。',
+  ];
+
+  const nextText = () => {
+    setCurrentTextIndex((prevIndex) => prevIndex + 1);
+  };
 
   useEffect(() => {
     async function checkRoomExists() {
@@ -51,7 +64,6 @@ export default function Home({ params }: { params: { slug: string } }) {
         const websocketUrl = `${process.env.NEXT_PUBLIC_BACKEND_WEBSOCKET_URL}/${params.slug}`;
         const websocket = new WebSocket(websocketUrl);
         setWs(websocket);
-        
 
         websocket.onmessage = (event) => {
           if (event.data == "goMystery") {
@@ -100,35 +112,73 @@ export default function Home({ params }: { params: { slug: string } }) {
 
   return (
     <div
-      className="h-screen w-full bg-cover"
+      className="h-screen w-full bg-cover flex justify-center items-center"
       style={{ backgroundImage: "url('/prologue.png')" }}
     >
       <GlitchEffect />
 
-      <div className="text-container">
-        <TextEffect></TextEffect>
-      </div>
+      {/* Story Texts */}
+      {currentTextIndex < storyTexts.length && (
+        <div
+          style={{
+            position: 'absolute', // This might be changed if needed
+            backgroundColor: 'rgba(0, 0, 0, 0.56)',
+            color: 'white',
+            padding: '20px',
+            borderRadius: '10px',
+            textAlign: 'center',
+            width: '800px',
+            maxHeight: '80vh',
+            overflowY: 'auto',
+            zIndex: 1000,
+          }}
+        >
+          <p style={{ marginBottom: '20px', height: '40px' }}>{storyTexts[currentTextIndex]}</p>
+          <div
+            style={{
+              position: 'absolute',
+              right: '10px',
+              bottom: '10px',
+              cursor: 'pointer',
+              fontSize: '24px',
+            }}
+            onClick={nextText}
+          >
+            ▼
+          </div>
+        </div>
+      )}
 
-   
-      <button className="clean-my-button" onClick={clickMysteryClean} type="button">
-        <WaitingRoomButton />
-        {hostWhichRoom === 'clean' && <span className="indicator-item indicator-start badge badge-secondary"></span>}
-        {guestWhichRoom === 'clean' && <span className="indicator-item badge badge-primary"></span>}
-      </button>
-    
-      <button className="dirty-my-button" onClick={clickMysteryDirty} type="button">
-        <WaitingRoomDirtyButton />
-        {hostWhichRoom === 'dirty' && <span className="indicator-item indicator-start badge badge-secondary"></span>}
-        {guestWhichRoom === 'dirty' && <span className="indicator-item badge badge-primary"></span>}
-      </button>
-    
-      {hostWhichRoom && guestWhichRoom && (hostWhichRoom !== guestWhichRoom) && (
-        <button onClick={goMystery} className="game-start" type="button">
-          ゲームスタート
-        </button>
+      {/* Buttons */}
+      {currentTextIndex >= storyTexts.length && (
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            zIndex: 1000,
+          }}
+        >
+          <button className="clean-my-button" onClick={clickMysteryClean} type="button">
+            <WaitingRoomButton />
+            {hostWhichRoom === 'clean' && <span className="indicator-item indicator-start badge badge-secondary"></span>}
+            {guestWhichRoom === 'clean' && <span className="indicator-item badge badge-primary"></span>}
+          </button>
+
+          <button className="dirty-my-button" onClick={clickMysteryDirty} type="button">
+            <WaitingRoomDirtyButton />
+            {hostWhichRoom === 'dirty' && <span className="indicator-item indicator-start badge badge-secondary"></span>}
+            {guestWhichRoom === 'dirty' && <span className="indicator-item badge badge-primary"></span>}
+          </button>
+
+          {hostWhichRoom && guestWhichRoom && (hostWhichRoom !== guestWhichRoom) && (
+            <button onClick={goMystery} className="game-start" type="button">
+              ゲームスタート
+            </button>
+          )}
+        </div>
       )}
     </div>
   );
-      }  
-
+}
 
