30c0e1c52e6eb142a1db5a8e9ae9acf7c7ae7986
neko967 <113199838+neko967@users.noreply.github.com>
2023-11-18 03:02:36 +0900

    ミザリーの過去部屋を作成

---
+++ src/app/misery/page.tsx
@@ -0,0 +1,181 @@
+'use client';
+import React, { useState, useEffect } from 'react';
+
+export default function Home() {
+const [currentTextIndex, setCurrentTextIndex] = useState(0);
+const [currentTextIndexBook1, setCurrentTextIndexBook1] = useState(2);
+const [currentTextIndexBook2, setCurrentTextIndexBook2] = useState(3);
+const [currentTextIndexBook3, setCurrentTextIndexBook3] = useState(4);
+const [ book1, setBook1 ] = useState(false);
+const [ book2, setBook2 ] = useState(false);
+const [ book3, setBook3 ] = useState(false);
+
+const storyTexts = [
+  'ミザリーの過去に招かれたという説明',
+];
+
+const storyTextsBook1 = [
+  'Book1の内容',
+  'Book1の内容2',
+];
+
+const storyTextsBook2 = [
+  'Book2の内容',
+  'Book2の内容2',
+  'Book2の内容3',
+];
+
+const storyTextsBook3 = [
+  'Book3の内容',
+  'Book3の内容2',
+  'Book3の内容3',
+  'Book3の内容4',
+];
+
+const nextText = () => {
+  // 最後のテキストを表示した後、もう一つインデックスを増やして
+  if (currentTextIndex < storyTexts.length) {
+    setCurrentTextIndex(currentTextIndex + 1);
+  }
+};
+
+const handleClickBook1 = () => {
+  setCurrentTextIndexBook1(0);
+  setBook1(true);
+};
+
+const nextTextBook1 = () => {
+  if (currentTextIndexBook1 < storyTextsBook1.length) {
+    setCurrentTextIndexBook1(currentTextIndexBook1 + 1);
+  }
+};
+
+const handleClickBook2 = () => {
+  setCurrentTextIndexBook2(0);
+  setBook2(true);
+};
+
+const nextTextBook2 = () => {
+  if (currentTextIndexBook2 < storyTextsBook2.length) {
+    setCurrentTextIndexBook2(currentTextIndexBook2 + 1);
+  }
+};
+
+const handleClickBook3 = () => {
+  setCurrentTextIndexBook3(0);
+  setBook3(true);
+};
+
+const nextTextBook3 = () => {
+  if (currentTextIndexBook3 < storyTextsBook3.length) {
+    setCurrentTextIndexBook3(currentTextIndexBook3 + 1);
+  }
+};
+
+return (
+  <div
+    className="relative h-screen w-full bg-cover"
+    style={{ backgroundImage: "url('/misery_past.png')" }}
+  >
+    {currentTextIndex < storyTexts.length && (
+      <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)',
+          backgroundColor: 'rgba(0, 0, 0, 0.56)', color: 'white', padding: '20px',
+          borderRadius: '10px', textAlign: 'center', width: '1000px',
+          maxHeight: '80vh', // 画面の高さの80%を最大に
+          overflowY: 'auto', // 縦方向にスクロール可能に
+          zIndex: 1000
+        }}
+      >
+        <p style={{ margin: '10px', height: '20px' }}>{storyTexts[currentTextIndex]}</p>
+        <div style={{ position: 'absolute', right: '10px', bottom: '10px', cursor: 'pointer', 
+                      fontSize: '24px', animation: 'bounce 1s infinite' }}
+             onClick={nextText}
+        >
+          ▼
+        </div>
+      </div>
+    )}
+
+    {currentTextIndexBook1 < storyTextsBook1.length && (
+      <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)',
+          backgroundColor: 'rgba(0, 0, 0, 0.56)', color: 'white', padding: '20px',
+          borderRadius: '10px', textAlign: 'center', width: '1000px',
+          maxHeight: '80vh', // 画面の高さの80%を最大に
+          overflowY: 'auto', // 縦方向にスクロール可能に
+          zIndex: 1000
+        }}
+      >
+        <p style={{ margin: '10px', height: '20px' }}>{storyTextsBook1[currentTextIndexBook1]}</p>
+        <div style={{ position: 'absolute', right: '10px', bottom: '10px', cursor: 'pointer', 
+                      fontSize: '24px', animation: 'bounce 1s infinite' }}
+             onClick={nextTextBook1}
+        >
+          ▼
+        </div>
+      </div>
+    )}
+
+    {currentTextIndexBook2 < storyTextsBook2.length && (
+      <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)',
+          backgroundColor: 'rgba(0, 0, 0, 0.56)', color: 'white', padding: '20px',
+          borderRadius: '10px', textAlign: 'center', width: '1000px',
+          maxHeight: '80vh', // 画面の高さの80%を最大に
+          overflowY: 'auto', // 縦方向にスクロール可能に
+          zIndex: 1000
+        }}
+      >
+        <p style={{ margin: '10px', height: '20px' }}>{storyTextsBook2[currentTextIndexBook2]}</p>
+        <div style={{ position: 'absolute', right: '10px', bottom: '10px', cursor: 'pointer', 
+                      fontSize: '24px', animation: 'bounce 1s infinite' }}
+             onClick={nextTextBook2}
+        >
+          ▼
+        </div>
+      </div>
+    )}
+
+    {currentTextIndexBook3 < storyTextsBook3.length && (
+      <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)',
+          backgroundColor: 'rgba(0, 0, 0, 0.56)', color: 'white', padding: '20px',
+          borderRadius: '10px', textAlign: 'center', width: '1000px',
+          maxHeight: '80vh', // 画面の高さの80%を最大に
+          overflowY: 'auto', // 縦方向にスクロール可能に
+          zIndex: 1000
+        }}
+      >
+        <p style={{ margin: '10px', height: '20px' }}>{storyTextsBook3[currentTextIndexBook3]}</p>
+        <div style={{ position: 'absolute', right: '10px', bottom: '10px', cursor: 'pointer', 
+                      fontSize: '24px', animation: 'bounce 1s infinite' }}
+             onClick={nextTextBook3}
+        >
+          ▼
+        </div>
+      </div>
+    )}
+
+    {currentTextIndex === storyTexts.length && currentTextIndexBook1 === storyTextsBook1.length && 
+     currentTextIndexBook2 === storyTextsBook2.length && currentTextIndexBook3 === storyTextsBook3.length && (
+      <>
+        <div
+          className="absolute left-1/4 top-3/4 h-48 w-48 bg-cover hover:rotate-6 z-10 translate-x-[calc(-50%)] translate-y-[calc(-50%)]"
+          style={{ backgroundImage: "url('/book.png')"}}
+          onClick={handleClickBook1}
+        >
+        </div>
+        <div
+          className="absolute left-1/2 top-3/4 h-48 w-48 bg-cover hover:rotate-6 z-10 translate-x-[calc(-50%)] translate-y-[calc(-50%)]"
+          style={{ backgroundImage: "url('/book.png')"}}
+          onClick={handleClickBook2}
+        >
+        </div>
+        <div
+          className="absolute left-3/4 top-3/4 h-48 w-48 bg-cover hover:rotate-6 z-10 translate-x-[calc(-50%)] translate-y-[calc(-50%)]"
+          style={{ backgroundImage: "url('/book.png')"}}
+          onClick={handleClickBook3}
+        >
+        </div>
+      </>
+    )}
+  </div>
+);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import Link from 'next/link'
-import { GetWindowSize } from "../hooks/GetWindowSize";
 
 type Room = {
   name: string;
@@ -12,8 +10,6 @@ type Room = {
 export default function Home() {
   const [rooms, setRooms] = useState<Room[]>([]);
   const router = useRouter();
-  const { height, width } = GetWindowSize();
-  
 
   async function createRoom() {
     var S="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -36,11 +32,18 @@ export default function Home() {
 
   async function joinRoom() {
     const name = prompt("招待コードを入力してください");
+    if (name === "MISERY") {
+      alert("ようこそ...");
+      router.push(`/misery`);
+      return;
+    }
+
     const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_HTTP_URL}/api/join-room/${name}`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ name }),
     });
+
     if (response.ok) {
       const data = await response.json();
       const roomName = data.name;
@@ -57,21 +60,21 @@ export default function Home() {
       style={{ backgroundImage: "url('/background.png')" }}
     >
       <div className="relative ml-0">
-      <div className="fixed top-[-30px] ml-10 flex animate-flicker-5 pt-0">
-        <div className="font-horror2 ml-4  text-[min(20vw,130px)]">M</div>
-        <div className="font-horror1 text-[min(20vw,130px)]">ISE</div>
-        <div className="font-horror2 text-[min(20vw,130px)]">R</div>
-        <div className="font-horror4 text-[min(20vw,130px)]">Y</div>
-      </div>
+        <div className="fixed top-[-30px] ml-10 flex animate-flicker-5 pt-0">
+          <div className="font-horror2 ml-4  text-[min(20vw,130px)]">M</div>
+          <div className="font-horror1 text-[min(20vw,130px)]">ISE</div>
+          <div className="font-horror2 text-[min(20vw,130px)]">R</div>
+          <div className="font-horror4 text-[min(20vw,130px)]">Y</div>
+        </div>
       </div>
       <div className="absolute top-80 right-90 bottom-60 gap-4 flex flex-col ">
         <button onClick={createRoom}
                 className="font-horror2 my-5 ml-10 mr-50  text-7xl">
-        NEW GAME
+          NEW GAME
         </button>
         <button onClick={joinRoom}
                 className="font-horror2 ml-20 mr-10  text-7xl">
-        JOIN ROOM
+          JOIN ROOM
         </button>
       </div>
       
