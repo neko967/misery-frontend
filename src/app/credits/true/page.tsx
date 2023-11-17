a2219a56afb33170aa5a016b8f6c483a6916959a
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2023-11-18 02:18:53 +0900

    新聞紙のギミック実装及びTrueエンドの実装

---
+++ src/app/credits/true/page.tsx
@@ -0,0 +1,83 @@
+'use client';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Home() {
+  const router = useRouter();
+
+  function TwitterIcon() {
+    return (
+      <svg viewBox="0 0 24 24" width="24" height="24">
+        <g>
+          <path d="M14.258 10.152L23.176 0h-2.113l-7.747 8.813L7.133 0H0l9.352 13.328L0 23.973h2.113l8.176-9.309 6.531 9.309h7.133zm-2.895 3.293l-.949-1.328L2.875 1.56h3.246l6.086 8.523.945 1.328 7.91 11.078h-3.246zm0 0" fill="#FFF" />
+        </g>
+      </svg>
+    );
+  }
+
+  return (
+    <div
+      className="h-screen w-full bg-cover"
+      style={{ backgroundImage: "url('/credits.png')" }}
+    >
+      <div className="thanks-message">
+        Thank You For Playing!!
+      </div>
+      <div className="bg-pink-500 text-white py-2 px-4 rounded shadow-lg">
+        テストテスト。MISERYを入力する旨のヒントをどこかに書く必要あり。
+      </div>
+     
+      {/* Twitterリンク */}
+      <button className="icon"
+      onClick={() => {
+        const tweetOptions = [
+          "この館の残影に、闇よりも深い秘密が眠る。",
+          "彼らが挑む、過去の鎖を断ち切る試練。",
+          "生ける者に告ぐ、彼女の闇に踏み入るな。",
+          "彼女の呪縛は永遠なり、その呼び声に応える者は…。",
+          "亡霊少女の遊戯、その終わりなき螺旋。",
+          "破滅を招く少女、その影が映すは真か謎か。",
+          "彼らの運命は交錯し、一人の少女と邂逅する。",
+          "館は語る、千年の孤独を、少女の絶望を。",
+          "館に響くは、禁断の詠唱。",
+          "屋敷の門を開く者、永遠の闇に囚われん。",
+          "逃れがたき呪縛の館、あなたは真実を見つけ出せるか。",
+          "この洋館に響くは、悲劇のメロディー。彼女の声が導く、終焉のダンスへ。",
+          "閉ざされた扉の向こうに、彼女は不死の孤独を紡ぐ。",
+          "遺されし館の呪い、静寂が叫びに変わる時。",
+          "暗闇に浮かぶ一筋の光、それは彼女の怨嗟か、救済か。",
+          "彼女の名を呼ぶは、禁断を犯すこと。",
+          "二人が目撃する闇は、時を超えし絶望の追憶。",
+          "古の呪いが呼び覚ます、彼女の叫びを此処で聴け。",
+          "館にうごめく、絶望の鎮魂歌。",
+          "閉じ込められた過去が今、現実を織りなす枷となる。",
+          "彼女の悲鳴は、夜の帳を切り裂く禁断の調べ。",
+          
+          
+        ];
+        const tweetText = tweetOptions[Math.floor(Math.random() * tweetOptions.length)];
+        const pageUrl = "https://misery-seven.vercel.app/";
+    
+        // Twitter Intentでテキストと画像URLを含めてツイート
+        window.open(
+          `https://twitter.com/share?url=${encodeURIComponent(pageUrl)}&text=${encodeURIComponent(tweetText)}
+          &hashtags=MISERY`,
+          '_blank' 
+        ); 
+      }}
+    >
+      <TwitterIcon />  
+    </button>
+
+      {/* トップページへのリンク */}
+      <Link href="/"
+        className="end-button">
+        TOPページに戻る
+      </Link>
+    </div>
+  );
+}
+
+
---
+++ src/app/ending/true/page.tsx
@@ -0,0 +1,75 @@
+'use client';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+export default function Home() {
+  const router = useRouter();
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+
+  const storyTexts = [
+    'Trueエンドのテストテストテスト暗闇を切り裂いたのは、明かりの灯る窓へと急ぐ二人の足音だけだった。扉を抜けた瞬間、洋館からの脱出を意味する新鮮な空気が二人を包む。',
+    '安堵の息を吐きながらも、心の片隅には、洋館で見た幻想のような一幕が残り続けていた。',
+    '「あの女の子はなんだったのだろう？」',
+    '探検を終えた安堵感に浸りつつも、二人の間には共有された未解決の疑問が浮かび上がる。彼らの背後で、そっと閉じる洋館の扉。',
+    '脱出した二人は、朝焼けが空をわずかに染め始める中、静けさの中を歩みを進める。一件の冒険が幕を閉じたが、あの女の子の謎だけが、今も彼らの心に静かに残る。',
+    'しかし、彼らは知っていた。あの洋館に隠された物語の断片を、もしかしたら決して全ては解き明かせないかもしれないと。',
+    '手を取り合い、生きた証を探し求めた二人の旅は、新たな物語の始まりを予感させながら、早朝の薄明かりの中、静かにその幕を閉じるのだった。',
+  ];
+
+  const nextText = () => {
+    setCurrentTextIndex((prevIndex) => prevIndex + 1);
+  };
+
+  async function goCreditsTrue() {
+    router.push(`/credits/true`);
+  }
+
+  return (
+    <div
+      className="h-screen w-full bg-cover flex justify-center items-center"
+      style={{ backgroundImage: "url('/ending.png')" }}
+    >
+      {/* Story Texts */}
+      {currentTextIndex < storyTexts.length && (
+        <div
+          style={{
+            position: 'relative', // これにより、内部のabsolute要素をこのdivに対して相対的に配置できる
+            backgroundColor: 'rgba(0, 0, 0, 0.56)',
+            color: 'white',
+            padding: '20px',
+            borderRadius: '10px',
+            textAlign: 'center',
+            width: '1050px',
+            maxHeight: '80vh',
+            overflowY: 'auto',
+          }}
+        >
+          <p style={{ margin: '10px', height: '20px' }}>{storyTexts[currentTextIndex]}</p>
+          <div
+            style={{
+              position: 'absolute', // これで右下に固定する
+              right: '10px', // 右端からの距離
+              bottom: '10px', // 下端からの距離
+              cursor: 'pointer',
+              fontSize: '24px',
+              animation: 'bounce 1s infinite'
+            }}
+            onClick={nextText}
+          >
+            ▼
+          </div>
+        </div>
+      )}
+
+      {/* Buttons */}
+      {currentTextIndex >= storyTexts.length && (
+        <button
+          onClick={goCreditsTrue}
+          className="end-button"
+        >
+          ▶▶ The End.
+        </button>
+      )}
+    </div>
+  );
+}
